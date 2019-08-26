30228bcdeefdd82956f495be4aa413e710f9774d
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-26 15:23:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,3 +14,24 @@ end
 def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+# def roll_call_dwarves(dwarves)
+#   dwarves.each_with_index do |item, index| 
+#     puts "#{index+1}.#{item}"
+#   end
+# end
+
+# def summon_captain_planet(planeteer_calls)
+#   planeteer_calls.map {|planeteer| planeteer.capitalize + '!'}
+# end
+
+# def long_planeteer_calls(calls_long)
+#   calls_long.any? {|calls_long| calls_long.length >4}
+# end
+
+# def find_the_cheese(contains_cheddar)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   contains_cheddar.find {|found_cheese| cheese_types.include? found_cheese}
+# end
+
